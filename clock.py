4121908061161b66b2071f62c15ac93bf734ad10
Joel Fattig <brfattig@gmail.com>
2019-10-29 18:39:33 -0500

    add Holocene era World Calendar
    use 1 CE as 9701 HE
    thus, 2000 CE is 11700 HE
    use latin names for months
    use World calendar as month lengths and leap days

--- clock.py
+++ clock.py
@@ -11,7 +11,7 @@ def toswatch(a):
     tot += int(m) * 60
     tot += int(s)
     tot /= 86.4
-    return round(tot, 2)
+    return tot
 
 
 def gettime():
@@ -20,3 +20,59 @@ def gettime():
 
 def tostr(a):
     return a.strftime("%H:%M:%S")
+
+def getdate():
+    return datetime.now().date()
+
+def toWorld(a):
+    d = a.strftime("%Y-%j")
+    (y,d) = d.split("-")
+    d = int(d)
+    year = int(y) + 9700
+    month = ""
+    if 1 <= d <= 31:
+        month = "Ianuarius"
+        day = d
+    elif 32 <= d <= 61:
+        month = "Februarius"
+        day = d - 31
+    elif 62 <= d <= 91:
+        month = "Martius"
+        day = d - 61
+    elif 92 <= d <= 121:
+        month = "Aprilis"
+        day = d - 91
+    elif 122 <= d <= 152:
+        if d == 122 and year % 4 == 0 and year % 1000 != 0:
+            month = "Leapyear day"
+            day = ""
+            d -= 1
+        else:
+            month = "Maius"
+            day = d - 121
+    elif 153 <= d <= 182:
+        month = "Iunius"
+        day = d - 152
+    elif 183 <= d <= 213:
+        month = "Iulius"
+        day = d - 182
+    elif 214 <= d <= 243:
+        month = "Augustus"
+        day = d - 213
+    elif 244 <= d <= 273:
+        month = "September"
+        day = d - 243
+    elif 274 <= d <= 304:
+        month = "October"
+        day = d - 273
+    elif 305 <= d <= 334:
+        month = "November"
+        day = d - 304
+    elif 335 <= d <= 364:
+        month = "December"
+        day = d - 334
+    elif d == 365:
+        month = "Worldsday"
+        day = ""
+    
+    return "{} {} {}".format(year, month, day)
--- clockgui.py
+++ clockgui.py
@@ -6,16 +6,19 @@ ctx = bimpy.Context()
 
 ctx.init(250, 150, "Clock")
 
-t = clock.gettime()
-beats = clock.toswatch(t)
-
-
 while(not ctx.should_close()):
     with ctx:
-        time.sleep(1/60)
         t = clock.gettime()
         beats = clock.toswatch(t)
-        bimpy.begin("Local Swatch Time")
+        bimpy.begin("Time")
         bimpy.set_window_font_scale(2)
         bimpy.text("@{:.2f}".format(beats))
         bimpy.end()
+
+        b = clock.getdate()
+        dates = clock.toWorld(b)
+        bimpy.begin("Date")
+        bimpy.set_window_font_scale(2)
+        bimpy.text("{}".format(dates))
+        bimpy.end()
+        time.sleep(1/60)
