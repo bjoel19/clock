23f0070678e72d6f0b6793465e020734ed7e1211
Joel Fattig <brfattig@gmail.com>
2019-11-02 20:41:24 -0500

    delete bat and powershell files

--- clock.bat
+++
@@ -1,6 +0,0 @@
-cd %~dp0
-
-%windir%\System32\WindowsPowerShell\v1.0\powershell.exe ^
-  -ExecutionPolicy ByPass ^
-  -NoExit ^
-  -Command ". ./find_conda.ps1; & $conda_path; cd ../../../; conda activate base; python clockgui.py"
--- find_conda.ps1
+++
@@ -1,8 +0,0 @@
-
-# list of possible conda locations:
-$conda_search = "$env:UserProfile\*conda3\shell\condabin\conda-hook.ps1",
-                "$env:LOCALAPPDATA\Continuum\*conda3\shell\condabin\conda-hook.ps1",
-                "C:\ProgramData\*conda3\shell\condabin\conda-hook.ps1"    
-
-$conda_path = Resolve-Path ($conda_search.where({Test-Path $_}) | Select-Object -first 1)
-echo $conda_path
